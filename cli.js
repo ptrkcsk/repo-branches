5fe84794b064e63f5b8a729ae16f7b9d1c369ccf
Patrik Csak <patrikcsak@rocketloans.com>
2019-02-12 11:13:02 -0500

    Use semicolons

--- cli.js
+++ cli.js
@@ -1,28 +1,28 @@
-const chalk = require('chalk')
-const globby = require('globby')
-const os = require('os')
-const path = require('path')
-const git = require('simple-git/promise')
-const { table } = require('table')
+const chalk = require('chalk');
+const globby = require('globby');
+const os = require('os');
+const path = require('path');
+const git = require('simple-git/promise');
+const { table } = require('table');
 
-;(async () => {
-  let [, , reposPath] = process.argv
+(async () => {
+  let [, , reposPath] = process.argv;
 
   if (!reposPath) {
-    console.error(chalk.red('Usage: BIN_NAME_TBD repos-path'))
-    process.exit(1)
+    console.error(chalk.red('Usage: BIN_NAME_TBD repos-path'));
+    process.exit(1);
   }
 
-  reposPath = reposPath.replace('~', os.homedir())
+  reposPath = reposPath.replace('~', os.homedir());
 
   const repos = await globby(
     path.resolve(__dirname, reposPath) + '/*',
     { onlyDirectories: true }
-  )
+  );
   const rows = repos.map(async repo => {
-    const gitRepo = git(repo)
-    await gitRepo.fetch()
-    const repoStatus = await gitRepo.status()
+    const gitRepo = git(repo);
+    await gitRepo.fetch();
+    const repoStatus = await gitRepo.status();
     const {
       ahead,
       behind,
@@ -34,34 +34,34 @@ const { table } = require('table')
       renamed,
       staged,
       tracking: trackingBranch
-    } = repoStatus
+    } = repoStatus;
 
     const dirty = created.length || deleted.length || modified.length ||
-      notAdded.length || renamed.length || staged.length
-    let trackingStatus = ''
+      notAdded.length || renamed.length || staged.length;
+    let trackingStatus = '';
 
     const branchName = currentBranch === 'master'
       ? chalk.blue(currentBranch)
-      : chalk.yellow(currentBranch)
-    const cleanliness = dirty ? chalk.red('Dirty') : chalk.green('Clean')
+      : chalk.yellow(currentBranch);
+    const cleanliness = dirty ? chalk.red('Dirty') : chalk.green('Clean');
 
-    if (ahead) trackingStatus += chalk.red(`${ahead} ahead`)
-    if (behind) trackingStatus += chalk.red(ahead ? `/${behind} behind` : `${behind} behind`)
-    if (ahead || behind) trackingStatus += chalk.red(` ${trackingBranch}`)
-    else trackingStatus += chalk.green('Up to date')
+    if (ahead) trackingStatus += chalk.red(`${ahead} ahead`);
+    if (behind) trackingStatus += chalk.red(ahead ? `/${behind} behind` : `${behind} behind`);
+    if (ahead || behind) trackingStatus += chalk.red(` ${trackingBranch}`);
+    else trackingStatus += chalk.green('Up to date');
 
     return [
       path.basename(repo),
       branchName,
       cleanliness,
       trackingStatus
-    ]
-  })
+    ];
+  });
 
   Promise.all(rows).then(rows => {
     console.log(table([
       ['Repo', 'Branch', 'Working Directory', 'Tracking Branch'],
       ...rows
-    ]))
-  })
-})().catch(console.error)
+    ]));
+  });
+})().catch(console.error);
